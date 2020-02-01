8d181a4977d4346147cae0405ff507b77b08bddf
OcWebb <ryanwebster0@gmail.com>
2020-02-01 14:37:32 -0500

    Testing vscode git workflow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+#test
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newtest.db'
 db = SQLAlchemy(app)
