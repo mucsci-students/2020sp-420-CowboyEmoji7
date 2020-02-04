c140c956c01656dfe153113fe8c2ac8f5d814d99
OcWebb <ryanwebster0@gmail.com>
2020-02-04 14:58:07 -0500

    Added skeleton for save functionality

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 from app_package import routes
