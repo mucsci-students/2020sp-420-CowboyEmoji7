5d77b841b58ef83977610e8f8b725cf4c0d4ab52
NatureElf <therealnatureelf@gmail.com>
2020-02-04 14:07:37 -0500

    Fix addClass and Save window bug

--- app_package/static/javascript/index.js
+++ app_package/static/javascript/index.js
@@ -1,19 +1,21 @@
 document.querySelector('.main-body').style.width = screen.availWidth;
 const container = document.querySelector('.main-body');
 
-document.querySelector('.navBar').style.height = '97%';
+document.querySelector('.navBar').style.height = '96%';
 
 Draggable.create('.draggable', {
     bounds: container
 });
 
 function addClass(){
+    document.getElementById('ClassInput').value = "";
     document.getElementById('AddClassForm').style.display = "block";
     document.getElementById('AddButton').classList.remove("non-active");
     document.getElementById('AddButton').classList.add("active");
 }
 
 function cancelAddClass(){
+    document.getElementById('ClassInput').value = "";
     document.getElementById('AddClassForm').style.display = "none";
     document.getElementById('AddButton').classList.remove("active");
     document.getElementById('AddButton').classList.add("non-active");
@@ -21,12 +23,14 @@ function cancelAddClass(){
 
 
 function openSaveBox() {
+    document.getElementById('saveInput').value = "";
     document.getElementById('saveForm').style.display = "block";
     document.getElementById('SaveButton').classList.remove("non-active");
     document.getElementById('SaveButton').classList.add("active");
 }
 
 function closeSaveBox() {
+    document.getElementById('saveInput').value = "";
     document.getElementById('saveForm').style.display = "none";
     document.getElementById('SaveButton').classList.remove("active");
     document.getElementById('SaveButton').classList.add("non-active");
