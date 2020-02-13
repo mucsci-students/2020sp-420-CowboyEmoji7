15e10b9770baecd13bd96e598335d62df99dab3d
Nate Remlinger <remlingernate@gmail.com>
2020-02-12 22:34:40 -0500

    Add documentation for source files
    Added file and function level documentation to every source file.
    Tried to throw in some "TODO"s anywhere where I saw room for
    improvement.

--- app_package/models.py
+++ app_package/models.py
@@ -1,8 +1,14 @@
+"""Contains representations of the data model."""
+
 from app_package import db, ma
 from datetime import datetime
 
-
 class ClassSchema(db.Model):
+    """Data model for representation of classes in current diagram.
+    
+    Use 'name' as primary key because names are unique identifiers
+    """
+
     name = db.Column(db.String(200), primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     x = db.Column(db.Integer())
@@ -11,7 +17,8 @@ class ClassSchema(db.Model):
     def __repr__(self):
         return '<Class %r>' % self.name
 
-
 class SaveSchema(ma.ModelSchema):
+    """Meta model used by flask-marshmallow in jsonification."""
+    
     class Meta:
         model = ClassSchema
--- app_package/routes.py
+++ app_package/routes.py
@@ -1,10 +1,22 @@
+"""Connects front-end to back-end.
+
+Contains routes through which requests from
+  the view are passed to interact with the data model.
+"""
+
 from app_package.models import ClassSchema, SaveSchema
 from flask import render_template, json, url_for, request, redirect, flash, Response
 from app_package import app, db
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    """Deals with requests to the base index.
+
+    On POST request, adds requested class and redirects
+      to itself to re-render with updated data
+    On GET request, renders the base index with current data
+    """
+
     if request.method == 'POST':
         # form tag 'content'
         class_name = request.form['class_name']
@@ -25,9 +37,13 @@ def index():
         classes = ClassSchema.query.order_by(ClassSchema.date_created).all()
         return render_template('index.html', classes=classes)
 
-
 @app.route('/delete/<string:name>')
 def delete(name):
+    """Deals with requests to remove a class.
+
+    Removes the requested class from database, if successful
+    """
+
     class_to_delete = ClassSchema.query.get_or_404(name)
 
     try:
@@ -37,21 +53,34 @@ def delete(name):
     except:
         return 'ERROR: Unable to delete Class'
 
-
 @app.route('/save/', methods=['POST'])
 def save():
+    """Deals with requests to save current data locally.
+
+    JSONify current data and return the result in a .json file
+      as an attachment--to download--with requested name
+    """
+
     name = request.form['save_name']
 
     classes = ClassSchema.query.all()
+
+    # Use flask-marshmallow to "jsonify" current data
     class_schema = SaveSchema(many=True)
     out = class_schema.dump(classes)
 
+    # Options utilized strictly for readability of the resulting file
     contents = json.dumps(out, ensure_ascii=False, indent=4)
     return Response(contents, mimetype="application/json", headers={"Content-disposition":"attachment; filename="+ name + ".json;"})
 
-
 @app.route("/load/", methods=['POST'])
 def load():
+    """Deals with requests to load pre-existing user data from a user's storage.
+
+    Clears current data, then loads file given by user
+    Then, adds each datum to the working database and
+      redirects to base index to re-render with loaded data
+    """
 
     Jfile = request.files['file']
     classes = ClassSchema.query.all()
