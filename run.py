4e25891b87e6a0125502211038ab6dd8d671af86
Ryan Webster <ryanwebster0@gmail.com>
2020-02-20 14:28:41 -0500

    - Changed functionality to use CMD class - added command history - Added help - Added tab completion

--- run.py
+++ run.py
@@ -1,37 +1,89 @@
-from app_package.cmd_func import cmd_start_website, cmd_add, cmd_list_classes, cmd_delete
-
-RUN_LOOP = True
-
-
-def exit_loop(argsList):
-    """Exits the application and displays goodbye message"""
-    global RUN_LOOP
-    RUN_LOOP = False
-    return ["Exiting"]
+import cmd
+from app_package.core_func import core_add, core_delete
+from app_package.models import ClassSchema
+from app_package import app
 
 
-# format is 'cmdname': function_name
-cmdDict = {
-    "add": cmd_add,
-    "delete": cmd_delete,
-    "remove": cmd_delete,
-    "exit": exit_loop,
-    "web": cmd_start_website,
-    "list": cmd_list_classes
-}
+class replShell(cmd.Cmd):
+    intro = 'Welcome to the UML editor shell.   Type help or ? to list commands.\n'
+    prompt = '(UML): '
+    file = None
 
+    def do_web(cmd, args):
+        'Starts the web app'
+        app.run(debug=False)
 
-while RUN_LOOP:
-    # 'UML: ' is printed before the area where users type
-    userInput = input("UML: ")
-    tokenInput = userInput.split()
-
-    if tokenInput:
-        firstArg = tokenInput.pop(0)
-        if firstArg in cmdDict:
-            result = cmdDict[firstArg](tokenInput)
-            for line in result:
-                print("- " + line)
+    def do_add(cmd, args):
+        """Accepts a single class name OR a list separated by spaces and adds them to the database
+    ex: add dog cat fish  <-- will add all three classes to database"""
+        argList = args.split()
+        if argList:
+            for name in argList:
+                if core_add(name):
+                    print('ERROR: Unable to add Class \'' + name + '\'')
+                else:
+                    print('Successfully added class \'' + name + '\'')
         else:
-            print("Unknown Command")
-            
+            print("Please provide a class name")
+
+    def do_delete(cmd, args):
+        """Accepts a single class name OR a list separated by spaces and removes them from the database
+    ex: delete dog cat fish  <-- will delete all three classes from database"""
+        argList = args.split()
+        if argList:
+            for name in argList:
+                if core_delete(name):
+                    print('ERROR: Unable to delete Class \'' + name + '\'')
+                else:
+                    print('Successfully deleted class \'' + name + '\'')
+        else:
+            print("Please provide a class name")
+
+    def do_list(cmd, args):
+        'Lists every class in the database'
+        classes = ClassSchema.query.order_by(ClassSchema.date_created).all()
+        listStr = ""
+
+        if not classes:
+            print("No Classes")
+
+        for classObj in classes:
+            # Code in if else prevents a comma from comming after the last element
+            if classObj == classes[-1]:
+                listStr += classObj.name
+            else:
+                listStr += (classObj.name + ", ")
+        
+        print(listStr)
+
+    def do_exit(self, args):
+        'Exits the UML shell'
+        print('Thank you for using our UML editor')
+        self.close()
+        return True
+
+    # ----- record and playback -----
+    def do_record(self, arg):
+        'Save future commands to filename:  RECORD rose.cmd'
+        self.file = open(arg, 'w')
+
+    def do_playback(self, arg):
+        'Playback commands from a file:  PLAYBACK rose.cmd'
+        self.close()
+        with open(arg) as f:
+            self.cmdqueue.extend(f.read().splitlines())
+
+    def precmd(self, line):
+        line = line.lower()
+        if self.file and 'playback' not in line:
+            print(line, file=self.file)
+        return line
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
+
+if __name__ == '__main__':
+    replShell().cmdloop()
