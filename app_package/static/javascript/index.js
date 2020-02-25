b42f5d5dfcf13d60a59344b56f00f5fad7b162df
Ethan Gingrich <ejgjag@gmail.com>
2020-02-25 14:32:51 -0500

    made the error messages disappear on-click

--- app_package/static/javascript/index.js
+++ app_package/static/javascript/index.js
@@ -153,3 +153,7 @@ function updateCoords(name) {
     xReq.setRequestHeader("Connection", "close");
     xReq.send(params);
 }
+
+function closeFlashMsg(){
+    document.getElementById("flashMsg").style.display = "none";
+}
