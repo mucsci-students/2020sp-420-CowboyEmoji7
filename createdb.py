5a54dc0469feded1c3c4d95cd871adf9ba54125d
Ryan Webster <ryanwebster0@gmail.com>
2020-03-30 15:16:03 -0400

    - removed '.css' from theme names

--- createdb.py
+++ createdb.py
@@ -11,10 +11,11 @@ db.create_all()
 themeNames = [f for f in listdir(themePath) if isfile(join(themePath, f))]
 for theme in themeNames:
     #Setting default theme
-    if (theme == 'Dark-Green.css'):
-        newTheme = Theme(name=theme, active=True)
+    nameFormatted = theme[:-4]
+    if (nameFormatted == 'Dark-Green'):
+        newTheme = Theme(name=nameFormatted, active=True)
     else:
-        newTheme = Theme(name=theme, active=False)
+        newTheme = Theme(name=nameFormatted, active=False)
     db.session.add(newTheme)
 
 db.session.commit()
